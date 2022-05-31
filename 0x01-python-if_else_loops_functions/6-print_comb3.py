98af7912aaedcd46cc26876a4d064ca5b5328e49
ouko <oukoezzy@gmail.com>
2022-05-31 15:29:00 +0300

    loops

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 10):
+for i in range(0, 9):
     for j in range((i+1), 10):
-        if (i is not 8) or (j is not 9):
-            print("{}{}, ".format(i, j), end="")
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
         else:
-            print("{}{}".format(i, j))
+            print("{:d}{:d}".format(i, j))
