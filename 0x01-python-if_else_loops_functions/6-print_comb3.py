5ecdeb3539755663169b3d6221ce97f940309142
ouko <oukoezzy@gmail.com>
2022-05-31 15:30:02 +0300

    loops

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 9):
-    for j in range((i+1), 10):
-        if i == 8 and j == 9:
-            print("{:d}{:d}".format(i, j))
-        else:
-            print("{:d}{:d}".format(i, j))
+for i in range(10):
+    for j in range(i + 1, 10):
+        print("{}".format(str(i) + str(j)), end="")
+        if int(str(i) + str(j)) < 89:
+            print(", ", end="")
+print("")
