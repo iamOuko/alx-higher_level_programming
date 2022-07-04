ac6c3c4115ca231485436ebd656b3e2be54c13d8
ouko <oukoezzy@gmail.com>
2022-07-04 18:45:46 +0300

    inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,16 +6,5 @@ Author: ouko
 
 
 def inherits_from(obj, a_class):
-    """A function that returns True if the object is
-    an instance of a class that inherited (directly or indirectly)
-    from the specified class;
-    otherwise False.
-
-    Args:
-        obj(any): object of the class
-        a_class(type): describes the class
-
-    """
-    if isinstance(type(obj), a_class) and type(obj) != a_class:
-        return True
-    return False
+    """returns true if obj is a subclass of a_class, otherwise false"""
+    return(issubclass(type(obj), a_class) and type(obj) != a_class)
