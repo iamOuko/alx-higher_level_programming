17b85c09ed9e3c98ff9089f4319f2dce4e50a660
ouko <oukoezzy@gmail.com>
2022-07-04 18:10:43 +0300

    inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+class MyList(list):
+    """A class MyList that inherits from list"""
+
+    def print_sorted(self):
+        """prints the list, but sorted (ascending sort)"""
+        print(sorted(self))
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class BaseGeometry:
+    """An empty class"""
+
+    def area(self):
+        """Raises an exception because...
+        area is not implemented
+
+        """
+        raise Exception("area() is not implemented")
