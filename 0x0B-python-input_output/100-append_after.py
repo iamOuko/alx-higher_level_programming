af850a1751c89f454c84e5504a1b636af8b50d6a
ouko <oukoezzy@gmail.com>
2022-07-05 12:49:45 +0300

    python input output

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 """
-function that inserts a line of text to a file, after each line containing a specific string
+Module 100-append_after
+
+Contains function that appends str after lines that include keyword
 """
 
 
 def append_after(filename="", search_string="", new_string=""):
-    '''module Search and update
-    '''
-    with open(filename, 'r+') as f:
-        lines = f.readlines()
-        i = 0
-        for line in lines:
-            if line.find(search_string) is not -1:
-                lines.insert(i + 1, new_string)
-            i += 1
+    """appends str after lines that include keyword (search_string)
+    1. recreate content in new_text by copying lines over
+    2. append new_string after lines if needed
+    3. overwrite file from beginning
+    """
+
+    with open(filename, mode="r+", encoding="utf-8") as f:
+        new_text = ""
+        for line in f:
+            new_text += line
+            if search_string in line:
+                new_text += new_string
         f.seek(0)
-        f.write("".join(lines))
+        f.write(new_text)
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-''' script that adds all arguments to a Python list, and then save them to a file
-'''
-import json
-import sys
+"""
+Module 9-add_item
 
+Contains function that adds and saves to Python obj to JSON file; loads objects
+
+# run with ./9-add_item.py
+#
+# cat add_item.json ; echo ""
+# expect output: []
+#
+# ./9-add_item.py some random args
+# cat add_item.json ; echo ""
+# expect output: ["some", "random", "args"]
+
+"""
+
+
+from sys import argv
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
+
 try:
-    py_list = load_from_json_file(filename)
+    existing_content = load_from_json_file(filename)
 except FileNotFoundError:
-    py_list = []
-finally:
-    for el in sys.argv[1:]:
-        py_list.append(str(el))
-    save_to_json_file(py_list, filename)
+    existing_content = []
+
+save_to_json_file(existing_content + argv[1:], filename)
